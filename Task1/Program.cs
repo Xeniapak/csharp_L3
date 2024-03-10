e302707858477d9357132e6d45979476bb22e842
Kseniya Pak <xeniapak@yandex.ru>
2024-03-10 12:35:45 +0300

    Added Lesson3:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿double CalculateFormula(double a, double b, double c, double d)
+{
+    double numenator = a * b;
+    double denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1.0, 2.0, 3.0, 4.0);
+System.Console.WriteLine(result);
+
+
